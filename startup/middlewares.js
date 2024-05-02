b7de9ccdfb93aefb3e5dbf5b96810eee9cd85476
Amin Mahmoodian <a.mahmoodiyan@gmail.com>
2024-05-02 18:19:07 +0200

    adding public folder for styles and other assets

--- startup/middlewares.js
+++ startup/middlewares.js
@@ -7,4 +7,5 @@ module.exports = function(app, express){
     app.use(express.json());
     app.use(helmet());
     app.use(express.urlencoded({ extended: true }));
+    app.use(express.static('public'));
 }
